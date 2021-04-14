ad8068abb6236380eb02fa90cf28b49884b7384f
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-14 19:35:02 +0300

    Init

---
+++ src/test/java/com/volodymyr/bush/advertboard/AdvertBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyr.bush.advertboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
