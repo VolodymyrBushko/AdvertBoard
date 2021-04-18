e398fd9355ef900abe7571aec348a4012c961f4e
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-18 17:54:26 +0300

    Added the UserController. Move user views

---
+++ src/main/java/com/volodymyr/bush/advertboard/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.volodymyr.bush.advertboard.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users/")
+public class UserController {
+
+}
