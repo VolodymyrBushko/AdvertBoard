685280e24299daa5ea506d82bd9b5217b5cd2523
Volodymyr <volodymyr.bushko@gmail.com>
2021-04-19 13:26:09 +0300

    Updated the info user view and the UserController

--- src/main/java/com/volodymyr/bush/advertboard/controllers/UserController.java
+++ src/main/java/com/volodymyr/bush/advertboard/controllers/UserController.java
@@ -1,12 +1,13 @@
 package com.volodymyr.bush.advertboard.controllers;
 
 import com.volodymyr.bush.advertboard.entities.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -28,5 +29,31 @@ public class UserController {
             return "user/index";
         }
         throw new RuntimeException("Error: index (GET)");
+    }
+
+    @GetMapping("{id}")
+    public String info(@PathVariable Long id, Model model) {
+        String url = String.format("%s%d", URL, id);
+        User user = restTemplate.getForObject(url, User.class);
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "user/info";
+        }
+        throw new RuntimeException("Error: info (GET)");
+    }
+
+    @PutMapping("{id}")
+    public String updateUser(@ModelAttribute User user, @PathVariable Long id) {
+        String url = String.format("%s%d", URL, id);
+        HttpEntity<User> request = new HttpEntity<>(user);
+        restTemplate.exchange(url, HttpMethod.PUT, request, Void.class);
+        return "redirect:/users/";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        String url = String.format("%s%d", URL, id);
+        restTemplate.delete(url);
+        return "redirect:/users/";
     }
 }
